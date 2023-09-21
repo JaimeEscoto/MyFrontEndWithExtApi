81b3c5791b87a76986ccc40478570b62c509f039
Jaime Escoto <jaime.escoto@hotmail.com>
2023-09-20 23:44:19 -0600

    Pendiente aplicar solid en las llamadas.
    Testing

--- js/project.js
+++ js/project.js
@@ -1,35 +1,151 @@
+function loadPage(id,albumId)
+{
+  if (id<0){
+        getUsers();
+  }
+  else
+  {
+    //cargar galeria
+    getUserbyId(id);
+    getAlbumsbyId(id,albumId);
+  }
+}
 
-
-function getUsers(params) {
-    fetch('https://jsonplaceholder.typicode.com/users/')
+function getPhotosUrlbyAlbumId(albumId)
+{
+    document.getElementById("Fotos").innerHTML = "";
+    //document.getElementById("Albums").innerHTML = "";
+    //console.log('https://jsonplaceholder.typicode.com/albums?userId='+id);
+    fetch('https://jsonplaceholder.typicode.com/photos?albumId='+albumId)
       .then(response => response.json())
-      .then(json => 
+      .then(data => 
+        { 
+            data.forEach(item => {
+                // Crea un elemento de card
+                const card = document.createElement('div');
+                card.classList.add('card');
+                card.style.width = '8rem';
+
+
+                // Crea el contenido de la card
+                card.innerHTML = `
+                  <div class="card-body">
+                  <img class="card-img-top" src="${item.thumbnailUrl}" alt="Card image cap">
+                    <p class="card-title"> ${item.title}  </p>
+                   
+                
+                  </div>
+                `;
+          
+                // Agrega la card al contenedor
+                const contenedor = document.getElementById('Fotos');
+                contenedor.appendChild(card);
+            }
+            )
+             
+        }
+  
+        )
+
+
+}
+
+function getAlbumsbyId(id)
+{
+    document.getElementById("Albums").innerHTML = "";
+    //console.log('https://jsonplaceholder.typicode.com/albums?userId='+id);
+    fetch('https://jsonplaceholder.typicode.com/albums?userId='+id)
+      .then(response => response.json())
+      .then(data => 
+        { 
+            data.forEach(item => {
+                // Crea un elemento de card
+                const card = document.createElement('div');
+                card.classList.add('card');
+                card.style.width = '18rem';
+
+
+                // Crea el contenido de la card
+                card.innerHTML = `
+                  <div class="card-body">
+                    <h5 class="card-title">: ${item.title} : </h5>
+                   
+                    <button type="button" class="btn btn-dark" onclick="getPhotosUrlbyAlbumId(${item.id})">
+			Ver Fotos
+		</button>
+                  </div>
+                `;
+          
+                // Agrega la card al contenedor
+                const contenedor = document.getElementById('Albums');
+                contenedor.appendChild(card);
+            }
+            )
+             
+        }
+  
+        )
+
+
+}
+function getUserbyId(id)
+{
+    console.log('https://jsonplaceholder.typicode.com/users/'+id);
+    fetch('https://jsonplaceholder.typicode.com/users/'+id)
+      .then(response => response.json())
+      .then(data => 
         {
+            //console.log(typeof data);
+            nam=JSON.parse(JSON.stringify(data)).name
+            //console.log(typeof JSON.parse(JSON.stringify(data)).name);
+            //document.getElementById('toptitle').innerText=nam;
             //console.log(typeof json);
            
             //console.log(typeof myJSON);
-            users = JSON.parse(JSON.stringify(json));
-            console.log( users.name);
-
-            
-                
-            //document.getElementById("datosPersona").innerHTML=users.length;
-
-            
-            
-            //myTable=document.getElementById("Table");
-
-            for (i = 0; i < users.length; i++){
-
-            
-                $("#Users").append('<div class="card" style="width: 18rem"> <img src="..." class="card-img-top" alt="..." /> <div class="card-body"> <h5 class="card-title">'+users[i].name+'</h5> <p class="card-text">username: '+users[i].username+' </p><p class="card-text">e-mail: '+users[i].email+'</p> <a href="#" class="btn btn-primary">Ver Galeria</a> </div></div>' 
-            //+
-            //'<td align="center" style="dislay: none;">' + users.alumnoUTP[i].apePaterno + '</td>'+
-            //'<td align="center" style="dislay: none;">' + users.alumnoUTP[i].edad + '</td>'+'</tr>'
-            );
-            }
-
-            
+             
         }
+  
         )
 }
+
+function getUsers() {
+    fetch('https://jsonplaceholder.typicode.com/users/')
+      .then(response => response.json())
+      .then(data => 
+        {
+            //document.getElementById('toptitle').innerText="Usuarios";
+            //console.log(typeof json);
+           
+            //console.log(typeof myJSON);
+            
+            data.forEach(item => {
+                // Crea un elemento de card
+                const card = document.createElement('div');
+                card.classList.add('card');
+                card.style.width = '18rem';
+          
+                // Crea el contenido de la card
+                card.innerHTML = `
+                  <div class="card-body">
+                    <h5 class="card-title">${item.name}</h5>
+                    <p class="card-text">username: ${item.username}</p>
+                    <p class="card-text">email: ${item.email}</p>
+                    <button type="button" class="btn btn-dark" onclick="getAlbumsbyId(${item.id})">
+			Ver Albums
+		</button>
+                  </div>
+                `;
+          
+                // Agrega la card al contenedor
+                const contenedor = document.getElementById('Users');
+                contenedor.appendChild(card);
+            }
+            )
+                
+         
+        }
+  
+        )
+}
+
+    
