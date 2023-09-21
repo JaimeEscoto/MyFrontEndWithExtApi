5808bd452ef15c9f341a3e189d30b73a96730a7a
Jaime Escoto <jaime.escoto@hotmail.com>
2023-09-20 20:33:12 -0600

    Agregando cards con datos de users
    bis

--- js/project.js
+++ js/project.js
@@ -1,19 +1,34 @@
 
 
-function miFuncion(params) {
-    fetch('https://jsonplaceholder.typicode.com/users/1')
+function getUsers(params) {
+    fetch('https://jsonplaceholder.typicode.com/users/')
       .then(response => response.json())
       .then(json => 
         {
             //console.log(typeof json);
            
             //console.log(typeof myJSON);
-            myParce = JSON.parse(JSON.stringify(json));
-            console.log( myParce.name);
+            users = JSON.parse(JSON.stringify(json));
+            console.log( users.name);
 
             
                 
-                document.getElementById("datosPersona").innerHTML=myParce.name;
+            //document.getElementById("datosPersona").innerHTML=users.length;
+
+            
+            
+            //myTable=document.getElementById("Table");
+
+            for (i = 0; i < users.length; i++){
+
+            
+                $("#Users").append('<div class="card" style="width: 18rem"> <img src="..." class="card-img-top" alt="..." /> <div class="card-body"> <h5 class="card-title">'+users[i].name+'</h5> <p class="card-text">username: '+users[i].username+' </p><p class="card-text">e-mail: '+users[i].email+'</p> <a href="#" class="btn btn-primary">Ver Galeria</a> </div></div>' 
+            //+
+            //'<td align="center" style="dislay: none;">' + users.alumnoUTP[i].apePaterno + '</td>'+
+            //'<td align="center" style="dislay: none;">' + users.alumnoUTP[i].edad + '</td>'+'</tr>'
+            );
+            }
+
             
         }
         )
